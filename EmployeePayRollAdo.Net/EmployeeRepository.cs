c69026eaa7a18d47ed26775ce8ecf17617054c67
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-08-03 20:43:15 +0530

    [SaiVijayaKumar] Creating Connection

---
+++ EmployeePayRollAdo.Net/EmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayRollAdo.Net
+{
+    class EmployeeRepository
+    {
+        public static string connectionString = @"Server=(localdb)\MSSQLLocalDB;Initial Catalog = payroll_services;";
+        //Createing conection object to connect with database
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}

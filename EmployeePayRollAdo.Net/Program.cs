b2b3d39838ab52f36610739bb52d25b3e728aeeb
Sai Vijayakumar Bollu <saivijayakumar.b@tvsnext.io>
2021-08-03 20:25:45 +0530

    [SaiVijayaKumar] Commiting Welcome Message

---
+++ EmployeePayRollAdo.Net/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayRollAdo.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tWelcome to Employee PayRoll Using ADO.NET");
+        }
+    }
+}
